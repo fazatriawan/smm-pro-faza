182870bcfa76d94dad8e6207a283f10a6b406063
fazatriawan <122349461+fazatriawan@users.noreply.github.com>
2026-04-03 13:37:22 +0000

    force rebuild

--- smm-pro/frontend/src/index.js
+++ smm-pro/frontend/src/index.js
@@ -5,3 +5,4 @@ import './components/Layout/Layout.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+// force rebuild Fri Apr  3 13:37:22 UTC 2026
