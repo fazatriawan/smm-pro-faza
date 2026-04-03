772cbe57f59c324fadf9bf1a9fed347f57739899
fazatriawan <122349461+fazatriawan@users.noreply.github.com>
2026-04-03 14:10:22 +0000

    fix frontend index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,7 @@
-const express = require('express');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './components/Layout/Layout.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
