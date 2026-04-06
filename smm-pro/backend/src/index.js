dcad1b2668af8555b99e977161411d17c0985b94
fazatriawan <122349461+fazatriawan@users.noreply.github.com>
2026-04-06 09:21:47 +0000

    fix duplicate startScheduler import

--- smm-pro/backend/src/index.js
+++ smm-pro/backend/src/index.js
@@ -19,9 +19,7 @@ const authMetaRoutes = require('./routes/authMeta');
 const authYoutubeRoutes = require('./routes/authYoutube');
 const authTwitterRoutes = require('./routes/authTwitter');
 const authThreadsRoutes = require('./routes/authThreads');
-const { startScheduler } = require('./cron/scheduler');
 const { startTokenScheduler } = require('./cron/tokenScheduler');
-
 const { startScheduler } = require('./services/schedulerService');
 const { initSocket } = require('./services/socketService');
 
